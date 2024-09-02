6b08472c96d3ee1ad45f518fe159439602ee77a8
Fabian Gander <fabian.gander@soulcode.ch>
2024-09-02 17:32:19 +0200

    feat(Product) Improving product component

--- src/canvas/Product/index.tsx
+++ src/canvas/Product/index.tsx
@@ -1,13 +1,10 @@
-import { ComponentProps } from '@uniformdev/canvas-next-rsc/component';
 import { Product } from './Product';
 
-export type ProductProps = ComponentProps<{
-  name: string;
-  description: string;
-  shopifyProducts: {
+export type ProductProps = {
+  shopifyProduct: {
     entities: [{ id: string }];
   };
-}>;
+};
 
 export const productMappings = {
   product: Product,
